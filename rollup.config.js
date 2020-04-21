99061f8cfa23886b8330a9fc814f895c21efcdcd
Joel Denning <joeldenning@gmail.com>
2020-04-21 17:29:24 -0600

    Adding unminified versions

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,14 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import packageJson from "./package.json";
 
-function createConfig(format, target) {
+function createConfig(format, target, minify) {
   let dir = format === "module" ? "esm" : format;
   dir += `/${target}`;
-
-  const angularCoreDir = target === "es2015" ? "fesm2015" : "fesm5";
 
   return {
     input: `./src/${target}/angular-core.js`,
     output: {
-      file: `${dir}/angular-core.min.js`,
+      file: `${dir}/angular-core${minify ? ".min" : ""}.js`,
       sourcemap: true,
       format,
       banner: `/* @angular/core@${packageJson.version} */`,
@@ -22,19 +20,24 @@ function createConfig(format, target) {
         browser: true,
       }),
       commonjs(),
-      // terser({
-      //   output: {
-      //     comments: /@angular\/core@/,
-      //   },
-      // }),
+      minify &&
+        terser({
+          output: {
+            comments: /@angular\/core@/,
+          },
+        }),
     ],
     external: ["rxjs", "rxjs/operators"],
   };
 }
 
 export default [
-  createConfig("module", "es5"),
-  createConfig("module", "es2015"),
-  createConfig("system", "es5"),
-  createConfig("system", "es2015"),
+  createConfig("module", "es5", true),
+  createConfig("module", "es5", false),
+  createConfig("module", "es2015", true),
+  createConfig("module", "es2015", false),
+  createConfig("system", "es5", true),
+  createConfig("system", "es5", false),
+  createConfig("system", "es2015", true),
+  createConfig("system", "es2015", false),
 ];
