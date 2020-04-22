daa1fc21c815997b3362085f882b75538ef6aba1
Joel Denning <joeldenning@gmail.com>
2020-04-22 11:30:03 -0600

    Adding resolved versions to ESM builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,27 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import packageJson from "./package.json";
 
-function createConfig(format, target, minify) {
+function createConfig({ format, target, minify, resolvedRxjs }) {
   let dir = format === "module" ? "esm" : format;
   dir += `/${target}`;
 
   return {
     input: `./src/${target}/angular-core.js`,
     output: {
-      file: `${dir}/angular-core${minify ? ".min" : ""}.js`,
+      file: `${dir}/angular-core${resolvedRxjs ? ".resolved" : ""}${
+        minify ? ".min" : ""
+      }.js`,
       sourcemap: true,
       format,
       banner: `/* @angular/core@${packageJson.version} */`,
+      paths: {
+        rxjs: resolvedRxjs
+          ? `https://cdn.jsdelivr.net/npm/@esm-bundle/rxjs@${resolvedRxjs}/esm/${target}/rxjs.min.js`
+          : "rxjs",
+        "rxjs/operators": resolvedRxjs
+          ? `https://cdn.jsdelivr.net/npm/@esm-bundle/rxjs@${resolvedRxjs}/esm/${target}/rxjs-operators.min.js`
+          : "rxjs/operators",
+      },
     },
     plugins: [
       resolve({
@@ -31,13 +41,62 @@ function createConfig(format, target, minify) {
   };
 }
 
-export default [
-  createConfig("module", "es5", true),
-  createConfig("module", "es5", false),
-  createConfig("module", "es2015", true),
-  createConfig("module", "es2015", false),
-  createConfig("system", "es5", true),
-  createConfig("system", "es5", false),
-  createConfig("system", "es2015", true),
-  createConfig("system", "es2015", false),
-];
+export default () => {
+  const rxjsDep = packageJson.devDependencies["rxjs"];
+  const resolvedRxjs = rxjsDep.slice(rxjsDep.lastIndexOf("@" + 1));
+
+  return [
+    createConfig({
+      format: "module",
+      target: "es5",
+      minify: true,
+      resolvedRxjs,
+    }),
+    createConfig({
+      format: "module",
+      target: "es5",
+      minify: false,
+      resolvedRxjs,
+    }),
+    createConfig({
+      format: "module",
+      target: "es5",
+      minify: true,
+      resolvedRxjs: false,
+    }),
+    createConfig({
+      format: "module",
+      target: "es5",
+      minify: false,
+      resolvedRxjs: false,
+    }),
+    createConfig({
+      format: "module",
+      target: "es2015",
+      minify: true,
+      resolvedRxjs,
+    }),
+    createConfig({
+      format: "module",
+      target: "es2015",
+      minify: false,
+      resolvedRxjs,
+    }),
+    createConfig({
+      format: "module",
+      target: "es2015",
+      minify: true,
+      resolvedRxjs: false,
+    }),
+    createConfig({
+      format: "module",
+      target: "es2015",
+      minify: false,
+      resolvedRxjs: false,
+    }),
+    createConfig({ format: "system", target: "es5", minify: true }),
+    createConfig({ format: "system", target: "es5", minify: false }),
+    createConfig({ format: "system", target: "es2015", minify: true }),
+    createConfig({ format: "system", target: "es2015", minify: false }),
+  ];
+};
