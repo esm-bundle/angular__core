5c3ed40a935c9832eaf21fb85faf7a3db7e1c57a
arturovt <arthurandrosovich@gmail.com>
2022-03-23 02:50:22 +0200

    Upgrade to Angular 13

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,5 +1,15 @@
 describe("@esm-bundle/angular-core", () => {
-  it("can load the System.register es2015 bundle", () => {
-    return System.import("/base/system/es2015/ivy/angular-core.min.js");
+  it("can load the System.register es2015 bundle", async () => {
+    const m = await System.import(
+      "/base/system/es2015/ivy/angular-core.min.js"
+    );
+    expect(m.ApplicationRef).toBeDefined();
+  });
+
+  it("can load the System.register es2020 bundle", async () => {
+    const m = await System.import(
+      "/base/system/es2020/ivy/angular-core.min.js"
+    );
+    expect(m.ApplicationRef).toBeDefined();
   });
 });
