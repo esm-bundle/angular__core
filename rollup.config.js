2bb81fdfed8af0bc2a9f7d49741ad8d60004cbc5
Steven Kolamkuzhiyil <stevenkolamkuzhiyil@gmail.com>
2024-01-19 14:23:28 +0100

    Upgrade to Angular 17

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,119 @@
-import { join } from "path";
-import { terser } from "rollup-plugin-terser";
-import packageJson from "@angular/core/package.json";
+import fs from "fs";
+import url from "url";
+import path from "path";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import { createEs2015LinkerPlugin } from "@angular/compiler-cli/linker/babel";
+import {
+  ConsoleLogger,
+  NodeJSFileSystem,
+  LogLevel,
+} from "@angular/compiler-cli";
 
-export default [
-  ...["2022"]
-    .map((ecma) => [
-      createConfig({ ecma, prod: false, format: "system" }),
-      createConfig({ ecma, prod: true, format: "system" }),
-      createConfig({ ecma, prod: false, format: "es" }),
-      createConfig({ ecma, prod: true, format: "es" }),
-    ])
-    .flat(),
-];
+const __dirname = new url.URL(".", import.meta.url).pathname;
+const packageJson = JSON.parse(
+  fs
+    .readFileSync(
+      path.resolve(__dirname, "node_modules/@angular/core/package.json"),
+    )
+    .toString(),
+);
 
-function createConfig({ ecma, prod, format }) {
+/** File system used by the Angular linker plugin. */
+const fileSystem = new NodeJSFileSystem();
+/** Logger used by the Angular linker plugin. */
+const logger = new ConsoleLogger(LogLevel.info);
+/**
+ * The linker plugin is used to make output files AOT compatible, so they don't
+ * require the `@angular/compiler` at runtime.
+ */
+const linkerPlugin = createEs2015LinkerPlugin({
+  fileSystem,
+  logger,
+  linkerJitMode: false,
+});
+
+const packages = ["2022"]
+  .map((ecma) => [
+    {
+      ecma,
+      angularPackage: "@angular/core/primitives/signals",
+      filename: "signals",
+      filepath: `primitives`,
+    },
+    {
+      ecma,
+      angularPackage: "@angular/core",
+      filename: "core",
+    },
+    {
+      ecma,
+      angularPackage: "@angular/core/rxjs-interop",
+      filename: "rxjs-interop",
+    },
+  ])
+  .flat();
+
+export default packages
+  .map(({ ecma, angularPackage, filename, filepath }) => [
+    createConfig({
+      ecma,
+      prod: false,
+      format: "system",
+      angularPackage,
+      filename,
+      filepath,
+    }),
+    createConfig({
+      ecma,
+      prod: true,
+      format: "system",
+      angularPackage,
+      filename,
+      filepath,
+    }),
+    createConfig({
+      ecma,
+      prod: false,
+      format: "es",
+      angularPackage,
+      filename,
+      filepath,
+    }),
+    createConfig({
+      ecma,
+      prod: true,
+      format: "es",
+      angularPackage,
+      filename,
+      filepath,
+    }),
+  ])
+  .flat();
+
+function createConfig({
+  ecma,
+  prod,
+  format,
+  angularPackage,
+  filename,
+  filepath,
+}) {
   const dir = (format === "es" ? "." : format) + `/es${ecma}/ivy`;
 
   return {
-    input: join(__dirname, `node_modules/@angular/core/fesm${ecma}/core.mjs`),
+    input: path.join(
+      __dirname,
+      `${[`node_modules/@angular/core/fesm${ecma}`, filepath, filename].filter((nonnull) => nonnull).join("/")}.mjs`,
+    ),
     output: {
-      file: `${dir}/angular-core.${prod ? "min." : ""}js`,
+      file: `${dir}/angular-${filename}.${prod ? "min." : ""}js`,
       format,
       sourcemap: true,
-      banner: `/* esm-bundle - @angular/core@${packageJson.version} - Ivy - ${format} format - Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at https://angular.io/license */`,
+      banner: `/* esm-bundle - ${angularPackage}@${packageJson.version} - Ivy - ${format} format - es${ecma} - Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at https://angular.io/license */`,
     },
     plugins: [
+      babel({ plugins: [linkerPlugin] }),
       prod &&
         terser({
           format: {
@@ -40,6 +129,11 @@ function createConfig({ ecma, prod, format }) {
           },
         }),
     ],
-    external: ["rxjs", "rxjs/operators"],
+    external: [
+      "rxjs",
+      "rxjs/operators",
+      "@angular/core/primitives/signals",
+      "@angular/core",
+    ],
   };
 }
